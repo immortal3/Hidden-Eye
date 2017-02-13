8e7bff71c066c1ec759f91190e6bfd2e49dec513
dippatelict <dip.patel.ict@gmail.com>
2017-02-13 13:48:27 +0530

    For converting string into 2 - bit char array

---
+++ DataChunk.py
@@ -0,0 +1,25 @@
+
+
+def asciiTo2bit(dataString):
+	#dataString = 'bigger string lol lol'
+	ascii_array = []
+
+	#converting string into ascii char array
+	for x in string:
+		ascii_array.append(ord(x))
+
+	#print (ascii_array)
+
+	ascii_array_2bit = []
+
+	#converting 8-bit ascii into 4 * 2-bit ascii
+	for x in ascii_array:
+		ascii_array_2bit.append(x >> 6)
+		ascii_array_2bit.append((x >>4) & 0x03)
+		ascii_array_2bit.append((x >>2) & 0x03)
+		ascii_array_2bit.append(x & 0x03)
+
+
+	#print (ascii_array_2bit)
+
+	return ascii_array_2bit
