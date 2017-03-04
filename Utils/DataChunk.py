cef898e585cc2261f2f8e494329a0dac89607c6a
immortal3 <dip.patel.ict@gmail.com>
2017-03-04 22:59:47 +0530

    adding support for pdf

--- Utils/DataChunk.py
+++ Utils/DataChunk.py
@@ -1,5 +1,20 @@
 
 
+#convert pdf into 2bits array/list
+def pdfTo2bit(PDFFilePath):
+	byte_array_2bit = []
+	with open(PDFFilePath,'rb') as f:
+		while True:
+			byte_array = f.read(1)
+			if not byte_array:
+				break
+			for x in byte_array:
+				byte_array_2bit.append(x >> 6)
+				byte_array_2bit.append((x >>4) & 0x03)
+				byte_array_2bit.append((x >>2) & 0x03)
+				byte_array_2bit.append(x & 0x03)
+	return byte_array_2bit
+
 def asciiTo2bit(dataString):
 	#dataString = 'bigger string lol lol'
 	ascii_array = []
